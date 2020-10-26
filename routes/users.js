9c0f7497be744c61b70c57e8521b1d20772d8411
jungwook <neity16@daum.net>
2020-10-26 19:46:12 +0900

    2020-10-26 prifile 작성

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-var profileController = require('../controllers/profile');
-var postController = require('../controllers/post');
+const profileController = require('../controllers/profile');
+const postController = require('../controllers/post');
 const userController = require("../controllers/user");
 const authMiddleware = require("../middlewares/auth");
 
@@ -14,9 +14,7 @@ router.get("/", function (req, res, next) {
 
 /* ------회원가입(signup) & 로그인(login)------ */
 // 회원가입
-
 router.post("/signup", userController.signUp);
-
 // 로그인
 router.post("/login", userController.signIn);
 
@@ -27,14 +25,14 @@ router.get("/post/post-part", (req, res) => {});
 // (0-전체[기본] / 1-디자인 / 2-서버 / 3-안드 / 4-ios / 5-웹 )
 router.get("/post/part/:part", (req, res) => {});
 // 특정 포스트 로딩
-router.get("/post/post/:id", postController.getPost());
+router.get("/post/post/:id", authMiddleware.checkToken, postController.getPost);
 // 포스트-좋아요(good)
-router.put("/post/good", postController.modifyGood());
+router.put("/post/good", authMiddleware.checkToken, postController.modifyGood);
 
 /* ———프로필(profile)——— */
 // 기본정보 불러오기
-router.get("/profile", profileController.getProfile());
+router.get("/profile", authMiddleware.checkToken, profileController.getProfile);
 // 프로필 등록
-router.post("/profile", profileController.saveProfile());
+router.post("/profile", authMiddleware.checkToken, profileController.saveProfile);
 
 module.exports = router;
