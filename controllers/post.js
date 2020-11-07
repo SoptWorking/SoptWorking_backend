d26109dcf6d8b7f076e8db02c290628d6b068990
jungwook <neity16@daum.net>
2020-11-08 02:46:53 +0900

    2020-10-08 response메시지 추가 Kjw

--- controllers/post.js
+++ controllers/post.js
@@ -10,13 +10,14 @@ const post = {
   getPartPost: async (req, res) => {
     const part = req.params.part;
     const result = await userModel.find({ part: part });
-    return res.status(200).send(util.success(200, { result: result }));
+    return res.status(200).send(util.success(200, resMessage.PART_POST_GET_SUCCESS,{ result: result }));
   },
   // 특정 포스트(1개) 로딩
   getPost: async (req, res) => {
     const id = req.params.id;
     const result = await userModel.findById(id);
-    return res.status(200).send(util.success(200, { result: result }));
+
+      return res.status(200).send(util.success(200,resMessage.SINGLE_POST_GET_SUCCESS,{ result: result }));
   },
   // 포스트-좋아요(good)
   modifyGood: async (req, res) => {
@@ -32,7 +33,7 @@ const post = {
     await result.save();
     return res
       .status(200)
-      .send(util.success(200, { likeCount: result.like.length }));
+      .send(util.success(200, resMessage.POST_LIKE_MODIFY_SUCCESS, { likeCount: result.like.length }));
   },
 };
 
